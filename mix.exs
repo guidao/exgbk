987e28d3cbfd161b7df5779cb4b547046b149960
王峰 <wangfeng01@qiniu.com>
2018-07-19 22:52:57 +0800

    fix test

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GBK.MixProject do
 
   def project do
     [
-      app: :gbk,
+      app: :exgbk,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
